9582bf077e7b21c786e7bf584e30236de91f0702
Ipan Rahman <ipan.rahman@finansia.com>
2022-02-03 23:06:19 +0700

    takeout casbin

--- internal/auth/model/casbin.go
+++ internal/auth/model/casbin.go
@@ -1,12 +1,12 @@
 package model
 
 type CasbinRule struct {
-	ID    uint   `gorm:"primaryKey;autoIncrement"`
-	PType string `gorm:"size:512;uniqueIndex:unique_index"`
-	V0    string `gorm:"size:512;uniqueIndex:unique_index"`
-	V1    string `gorm:"size:512;uniqueIndex:unique_index"`
-	V2    string `gorm:"size:512;uniqueIndex:unique_index"`
-	V3    string `gorm:"size:512;uniqueIndex:unique_index"`
-	V4    string `gorm:"size:512;uniqueIndex:unique_index"`
-	V5    string `gorm:"size:512;uniqueIndex:unique_index"`
+	ID    uint   `gorm:"column:id;primaryKey;autoIncrement"`
+	PType string `gorm:"column:ptype;size:512;uniqueIndex:unique_index"`
+	V0    string `gorm:"column:v0;size:512;uniqueIndex:unique_index"`
+	V1    string `gorm:"column:v1;size:512;uniqueIndex:unique_index"`
+	V2    string `gorm:"column:v2;size:512;uniqueIndex:unique_index"`
+	V3    string `gorm:"column:v3;size:512;uniqueIndex:unique_index"`
+	V4    string `gorm:"column:v4;size:512;uniqueIndex:unique_index"`
+	V5    string `gorm:"column:v5;size:512;uniqueIndex:unique_index"`
 }
